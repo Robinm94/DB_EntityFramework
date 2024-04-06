cbcc5bb0f2858d621099d398ca4eed55d2594f15
robinm2812 <robinm2812@gmail.com>
2024-04-06 17:29:56 -0400

    Main program update

--- Week10Lab/Program.cs
+++ Week10Lab/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Week10Lab
+﻿using Week10Lab.Models.Entities;
+
+namespace Week10Lab
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            MMABooksContext context = new MMABooksContext();
+            foreach (var customer in context.Customers.ToList())
+            {
+                Console.WriteLine($"{customer.Name}");
+            }
         }
     }
 }
